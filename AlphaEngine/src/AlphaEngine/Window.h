9b7b3eed6ebda8dbb1aad97e338e8191463f9791
VWU6415 <vinit.james@volkswagen.de>
2020-04-12 18:15:34 +0200

    Added Window base class

---
+++ AlphaEngine/src/AlphaEngine/Window.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Aepch.h"
+#include "AlphaEngine/Core.h"
+#include "AlphaEngine/Events/Event.h"
+
+namespace AlphaEngine {
+
+	struct WindowProps {
+		std::string Title;
+		unsigned int Width;
+		unsigned int Height;
+
+		WindowProps(const std::string& title = "Alpha Engine",
+			         unsigned int width = 1200,
+			         unsigned int height = 720)
+			: Title(title), Width(width), Height(height) {}
+
+	};
+
+	class AlphaEngine_API Window {
+	public:
+		using EventCallbackFunc = std::function<void(Event&)>;
+
+		virtual ~Window(){}
+
+		virtual void OnUpdate() = 0;
+		virtual unsigned int GetWidth() const = 0;
+		virtual unsigned int GetHeight() const = 0;
+
+		// windows attributes
+		virtual void SetEventCallback(const EventCallbackFunc& func) = 0;
+		virtual void SetVSync(boolean enabled) = 0;
+		virtual bool IsVSync() const = 0;
+
+		static Window* Create(const WindowProps& windowprop = WindowProps());
+	};
+}
